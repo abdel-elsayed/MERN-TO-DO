edd88e2b38b410351c537f428e104df7c74861a3
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-01-17 16:06:47 -0500

    initial commit.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>hello</h1>
+      </div>
+    )
+  }
+}
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports ={
+
+    mongoURI: 'mongodb+srv://dbUser1:Mamanana98.@cluster0.chdzt.mongodb.net/Cluster0?retryWrites=true&w=majority'
+}
