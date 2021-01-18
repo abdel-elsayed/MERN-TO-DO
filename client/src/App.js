860a94ee769bd922d20fe9587e7d7b434ea5c517
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-01-18 12:15:13 -0500

    Finished redux and input modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import ShoppingList from './components/ShoppingList'
+import { Container } from 'reactstrap'
+import { Provider } from 'react-redux'
+import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import ItemModal from './components/ItemModal'
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <NavBar />
+         <Container>
+          <ItemModal />
+          <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
     )
   }
 }
