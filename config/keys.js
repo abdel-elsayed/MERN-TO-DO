62ff85a379967e614957a3cf8b8a53929d491230
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-01-18 20:24:55 -0500

    Fix backend.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports ={
 
-    mongoURI: 'mongodb+srv://dbUser1:Mamanana98.@cluster0.chdzt.mongodb.net/Cluster0?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://dbUser1:evW2LeKnAJTh7aS1@cluster0.chdzt.mongodb.net/Cluster0?retryWrites=true&w=majority'
 }
